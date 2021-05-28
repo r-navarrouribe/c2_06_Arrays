77c2a7f7c29252432a513d46e0279e283cf84e7e
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-05-28 13:45:42 +0200

    Ejercicio

---
+++ js/index.js
@@ -0,0 +1,99 @@
+const pacientes = [
+  {
+    paciente: {
+      nombre: "Manuel",
+      edad: 25,
+      sexo: "H",
+    },
+    diasIngresado: 3,
+    dieta: "Baja en fibra",
+  },
+  {
+    paciente: {
+      nombre: "Marta",
+      edad: 56,
+      sexo: "M",
+    },
+    diasIngresado: 5,
+    dieta: "Diabetes",
+  },
+  {
+    paciente: {
+      nombre: "Julia",
+      edad: 38,
+      sexo: "M",
+    },
+    diasIngresado: 1,
+    dieta: "Sin sal",
+  },
+  {
+    paciente: {
+      nombre: "Esteban",
+      edad: 40,
+      sexo: "H",
+    },
+    diasIngresado: 3,
+    dieta: "Diabetes",
+  },
+  {
+    paciente: {
+      nombre: "Arturo",
+      edad: 19,
+      sexo: "H",
+    },
+    diasIngresado: 2,
+    dieta: "Baja en fibra",
+  },
+  {
+    paciente: {
+      nombre: "Isabel",
+      edad: 63,
+      sexo: "M",
+    },
+    diasIngresado: 6,
+    dieta: "Sin sal",
+  },
+];
+
+// nPacientes
+const cantidadPacientes = pacientes.length;
+
+// nMayoresEdad
+const pacientesMayores = pacientes.filter(
+  (paciente) => paciente.paciente.edad >= 18
+);
+
+const cantidadPacientesMayores = pacientesMayores.length;
+
+// nHombresDiabeticos
+
+const hombresDiabeticos = pacientes.filter(
+  (paciente) => paciente.paciente.sexo === "H" && paciente.dieta === "Diabetes"
+);
+
+const cantidadHombresDiabeticos = hombresDiabeticos.length;
+
+// totalDiasIngreso
+
+const numeroTotalDiasIngreso = pacientes.reduce(
+  (acumulador, paciente) => acumulador + paciente.diasIngresado,
+  0
+);
+
+// mediaEdadMujeres
+
+const EdadMujeresMedia = pacientes
+  .filter((paciente) => paciente.paciente.sexo === "M")
+  .reduce((acumulador, paciente) => acumulador + paciente.paciente.edad, 0);
+
+// Creación objeto
+
+const objetoFinal = {
+  nPacientes: cantidadPacientes,
+  nMayoresEdad: pacientesMayores,
+  nHombresDiabeticos: hombresDiabeticos,
+  totalDiasIngreso: numeroTotalDiasIngreso,
+  mediaEdadMujeres: EdadMujeresMedia,
+};
+
+console.log(objetoFinal);
